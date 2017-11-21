ec0b795b0955d3d3d015b230d0c9a34fb434b286
Tina1397635331 <1397635331@qq.com>
2017-11-21 18:07:27 +0800

    车知道项目
    2017/11/21

---
+++ check_seal/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by gcg on 2017/11/20.
+ */
+//轮播图
+
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: 2000,//可选选项，自动滑动
+    paginationClickable: true,
+    // 如果需要分页器
+    // pagination: '.swiper-pagination',
+    loop:true
+});
